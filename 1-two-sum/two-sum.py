80df402cf8f01e5a5a996f7923f8f3b6fc9e722e
AmarjithTK <amarjithraveendran@gmail.com>
2026-07-07 22:18:58 +0530

    Time: 4 ms (41.41%) | Memory: 20.6 MB (17.74%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self,nums,target):
+        mp = {}
+
+
+        for i,x in enumerate(nums):
+            if target-x in mp:
+                return [mp[target-x],i]
+            mp[x]=i
+
+
+            
+        
